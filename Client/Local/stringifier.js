130c4415319817c00696792ded8f9265ef116bf9
GGBRW <brouwer.gees@gmail.com>
2016-09-13 10:41:53 +0200

    Create stringifier.js

---
+++ Client/Local/stringifier.js
@@ -0,0 +1,61 @@
+function stringify() {
+    let result = {
+      components: [],
+      connections: []
+    }
+    
+    for(let i of components) {
+        let component = [
+            i.constructor.name
+        ]
+        
+        // Params
+        component[1] = Object.assign({}, i);
+        delete component[1].input;
+        delete component[1].output;
+        delete component[1].from;
+        delete component[1].to;
+        
+        // Connections
+        if(i.input) {
+          for(let input of i.input) {
+            result.connections.push([
+                components.indexOf(input.from), // From
+                components.indexOf(input.to), // To
+                components.indexOf(input) // Wire
+            ]);
+          }
+        }
+        
+        result.components.push(component);
+    }
+    
+    return JSON.stringify(result);
+}
+
+function parse(string) {
+    string = JSON.parse(string);
+    for(let i of string.components) {
+        let component = eval("new " + i[0]);
+        Object.assign(component,i[1]);
+        components.push(component);
+    }
+    
+    for(let i of string.connections) {
+        const from = components[i[0]];
+        const to = components[i[1]];
+        const wire = components[i[2]];
+        
+        wire.from = from;
+        wire.to = to;
+        from.connect(to,wire);
+    }
+}
+
+function download(string) {
+    a = document.createElement("a");
+    data = "data:text/json;charset=utf-8," + encodeURIComponent(string);
+    a.setAttribute('href', data);
+    a.setAttribute('download', 'data.txt');
+    a.click();
+}
