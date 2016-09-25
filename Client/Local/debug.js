ed864804fe9c1dc040188062aa95619db2972de5
GGBRW <gees@ggbrw.nl>
2016-09-25 23:01:40 +0200

    Heel veel verbeteringen

--- Client/Local/debug.js
+++ Client/Local/debug.js
@@ -1,25 +1,48 @@
 const debugInfo = document.getElementById("debugInfo");
+debugInfo.addLine = function(name,val) {
+    let value = document.createElement("span");
+    value.style.float = "right";
+    switch(typeof val) {
+        case "number":
+            value.style.color = "#888";
+            break;
+        case "string":
+            value.style.color = "#888";
+            value.style.fontStyle = "italic";
+            break;
+        case "boolean":
+            if(val) value.style.color = "#060";
+            else value.style.color = "#600";
+            value.style.fontWeight = 800;
+            break;
+    }
+    value.innerHTML = val;
+
+    this.innerHTML += name + ": " + value.outerHTML + "<br>";
+}
 
 function updateDebugInfo() {
     if(settings.show_debugInfo) {
         debugInfo.style.display = "block";
-        debugInfo.innerHTML = "Framerate: " + Math.round(framerate) + "<br>";
-        debugInfo.innerHTML += "Screen res: " + c.width + "*" + c.height + "<br>";
-        debugInfo.innerHTML += "Canvas focus: " + (c == document.activeElement) + "<br>";
-        debugInfo.innerHTML += "Mouse screen x: " + Math.round(mouse.screen.x) + "<br>";
-        debugInfo.innerHTML += "Mouse screen y: " + Math.round(mouse.screen.y) + "<br>";
-        debugInfo.innerHTML += "Mouse grid x: " + Math.round(mouse.grid.x) + "<br>";
-        debugInfo.innerHTML += "Mouse grid y: " + Math.round(mouse.grid.y) + "<br>";
-        debugInfo.innerHTML += "Offset x: " + Math.round(offset.x) + "<br>";
-        debugInfo.innerHTML += "Offset y: " + Math.round(offset.y) + "<br>";
-        debugInfo.innerHTML += "Zoom: " + Math.round(zoom) + "<br>";
-        debugInfo.innerHTML += "Dragging: " + !!dragging + "<br>";
-        debugInfo.innerHTML += "Selecting: " + !!selecting + "<br>";
-        debugInfo.innerHTML += "Connecting: " + !!connecting + "<br>";
-        debugInfo.innerHTML += "Components: " + components.length + "<br>";
-        debugInfo.innerHTML += "Selected: " + Selected.name + "<br>";
-        debugInfo.innerHTML += "Ticks/sec: " + Math.round(tickrate) + "<br>";
-        debugInfo.innerHTML += "Updates: " + update_queue.length + "<br>";
+        debugInfo.innerText = "";
+
+        debugInfo.addLine("Framerate", Math.round(framerate));
+        debugInfo.addLine("Screen res", c.width + "*" + c.height);
+        debugInfo.addLine("Canvas focus", c == document.activeElement);
+        debugInfo.addLine("Mouse screen x", Math.round(mouse.screen.x));
+        debugInfo.addLine("Mouse screen y", Math.round(mouse.screen.y));
+        debugInfo.addLine("Mouse grid x", Math.round(mouse.grid.x));
+        debugInfo.addLine("Mouse grid y", Math.round(mouse.grid.y));
+        debugInfo.addLine("Offset x", Math.round(offset.x));
+        debugInfo.addLine("Offset y", Math.round(offset.y));
+        debugInfo.addLine("Zoom", Math.round(zoom));
+        debugInfo.addLine("Dragging", !!dragging);
+        debugInfo.addLine("Selecting", !!selecting);
+        debugInfo.addLine("Connecting", !!connecting);
+        debugInfo.addLine("Components", components.length);
+        debugInfo.addLine("Selected", Selected.name);
+        debugInfo.addLine("Ticks/sec", Math.round(tickrate));
+        debugInfo.addLine("Updates", update_queue.length);
         debugInfo.innerHTML += "<br><span style='font-family: \"Roboto Condensed\"; opacity: .5'>Hold F3 to hide this</span>";
     } else {
         debugInfo.style.display = "none";
